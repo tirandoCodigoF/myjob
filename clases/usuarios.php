e1a02845264d6c8faef138eef29db574d112519e
vicente perez santiago <virgo_santiago@live.com.mx>
2018-04-18 11:11:50 -0500

    continuando con las clases, usuarios

---
+++ clases/usuarios.php
@@ -0,0 +1,25 @@
+<?php 
+class usuarios{
+public $id;
+public $usuario;
+function get_id(){
+return $this->id;
+}
+function set_id($id){
+	$this->id=$id;
+}
+
+function get_usuario(){
+	this->usuario;
+}
+
+function set_usuario($usuario){
+	$this->usuario=$usuario;
+}
+
+
+
+
+}
+
+ ?>
