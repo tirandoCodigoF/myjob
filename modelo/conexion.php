937de8ba4acf2ca983a4b2934ecc1c0ea1edbffa
vicente perez santiago <virgo_santiago@live.com.mx>
2018-04-18 09:51:37 -0500

    terminada la conexion.php en modelo

---
+++ modelo/conexion.php
@@ -0,0 +1,18 @@
+<?php 
+class conexion{
+
+function conectar(){
+$conectar=new mysqli ("localhost","root","");
+return $conectar;  
+}
+
+}
+$conect=new conexion();
+if($conect->conectar()){
+	echo "conectado";
+}
+else {
+	echo"desconectado";
+}
+
+ ?>
