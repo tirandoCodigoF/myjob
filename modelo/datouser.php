2092c2133413da4ab37a0a455fc9458aa099fbac
vicente perez santiago <virgo_santiago@live.com.mx>
2018-04-18 12:15:01 -0500

    datos user continuando con datouser.php

--- modelo/datouser.php
+++ modelo/datouser.php
@@ -1,8 +1,8 @@
 <?php 
 include("../clases/usuarios.php");
-include "conexion.php";
+include ("conexion.php");
 //datos del usuario
-class datos(){
+class datouser {
 function insertar($usuario,$password){
 	$conec=new conexion();
 	$conect=$conec->conectar();
@@ -10,20 +10,20 @@ $usuarios= new usuarios();
 $usuarios->usuario=$usuario;
 $usuarios->password=$password;
 
-
+mysqli_select_db($conect,"loginorientado");
+$sql="insert into usuarios(usuario,password) values('".$usuarios->usuario."','".$usuarios->password."')";
+if (mysqli_query($conect,$sql)){
+	return true;
+}else{
+return false;
+}
+mysqli_close($conect);
+}
+$obj = new datouser();
+if($obj->insertar("vicente","samuragi23")){
+	echo "registro realizado";
 }
 
-
-
-
-
-
 }
-
-
-
-
-
-
 
  ?>
