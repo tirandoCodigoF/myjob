03f267e2a547dcd6a7ad4bc21e8593f023890f15
vicente perez santiago <virgo_santiago@live.com.mx>
2018-04-18 17:16:50 -0500

    finalizado la clase datos del usuario donde se registra ala bdd,datouser

--- modelo/datouser.php
+++ modelo/datouser.php
@@ -2,16 +2,15 @@
 include("../clases/usuarios.php");
 include ("conexion.php");
 //datos del usuario
-class datouser {
-function insertar($usuario,$password){
+class datouser{
+function insertar($usuario,$pass){
 	$conec=new conexion();
 	$conect=$conec->conectar();
-$usuarios= new usuarios();
-$usuarios->usuario=$usuario;
-$usuarios->password=$password;
-
+$usuarios = new usuarios();
+$usuarios->usuario = $usuario;
+$usuarios->password = $pass;
 mysqli_select_db($conect,"loginorientado");
-$sql="insert into usuarios(usuario,password) values('".$usuarios->usuario."','".$usuarios->password."')";
+$sql=" INSERT INTO usuarios(usuario,password) values('".$usuarios->usuario."','".$usuarios->password."')";
 if (mysqli_query($conect,$sql)){
 	return true;
 }else{
@@ -19,11 +18,12 @@ return false;
 }
 mysqli_close($conect);
 }
-$obj = new datouser();
-if($obj->insertar("vicente","samuragi23")){
-	echo "registro realizado";
-}
 
 }
+// $obj = new datouser();
+// if($obj->insertar("chanty","12345")){
+// 	echo "registro realizado";//para ejecutar la insercion del usuario y passwor en la base de datos.
+
+// }
 
  ?>
