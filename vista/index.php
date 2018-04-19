9a26e19e22953f2ecf2456d0487c36644f02fe85
vicente perez santiago <virgo_santiago@live.com.mx>
2018-04-19 09:59:22 -0500

    logueo de inicio de sesion y estilos

--- vista/index.php
+++ vista/index.php
@@ -1,15 +1,37 @@
-<?php 
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+     <script type="text/javascript" src="js/code.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/estilos.css">
+	<title>inciar sesion</title>
 </head>
 <body>
+ <div class="contenedor">
+<br>
+      <form  align="center" class="formulario">
+        <h2 class="form-signin-heading" align="center">Iniciar Sesion</h2>
+   <br>
+        <input type="text" id="usuario" class="form-control" placeholder="USUARIO:" required autofocus>
+       <br>
+        <input type="password" id="pass" class="form-control" placeholder="CONTRASEÑA" required>
+        <br>
+        <button class="btn btn-lg btn-primary btn-block" type="button">Ingresar</button>
+      </form>
+
+    </div> <!-- /contenedor -->
+
 
 </body>
 </html>
 
 
 
-
- ?>
