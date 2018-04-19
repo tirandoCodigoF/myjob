fe6452aa0c441f247b0fea6d29990c7baa421119
vicente perez santiago <virgo_santiago@live.com.mx>
2018-04-18 23:02:21 -0500

    finaliza archivo usercontrol.php del controlador

--- controlador/usercontrol.php
+++ controlador/usercontrol.php
@@ -1,3 +1,15 @@
 <?php 
+include "../modelo/datouser.php"
+//clase que se implementa para llamar al metodo insertar q se encuentra en la carpeta modelo.
+class usercontrol{
+function insertar($usuario,$pass){
+$obj = new datouser();
+return $obj->insertar($usuario,$pass);
+
+}
+
+
+}
+
 
  ?>
